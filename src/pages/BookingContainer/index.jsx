9aa175ad49dc27c7a703780dbcc7963f27461854
Timmy Chan <timmychan301197@gmail.com>
2023-04-18 19:56:37 +0200

    Add home page sketch

---
+++ src/pages/BookingContainer/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function BookingContainer() {
+  return (
+    <div className="relative flex w-5/6 justify-center align-top border-gray-500 p-6 rounded-xl border border-2 bg-gray-200">
+      
+        hello from booking container
+    </div>
+  )
+}
---
+++ src/pages/Header/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <div className="relative flex w-full justify-center p-4 text-xl">
+      Netcompany Seat booking service
+    </div>
+  )
+}
